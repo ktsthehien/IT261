4cdc054df0233a160d1d1f9a1c9f2a55e7dac3dc
Hien Nguyen <72104238+ktsthehien@users.noreply.github.com>
2022-03-22 23:49:54 -0700

    Add files via upload

--- final/includes/header-no-nav.php
+++ final/includes/header-no-nav.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
 <meta charset="UTF-8">
-<title>Temporary title</title>
+<title><?php echo $title;?></title>
 <link href="css/styles.css" type="text/css" rel="stylesheet">
 </head>
 
